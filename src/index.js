49c46599658a1127c47bb16e714428990a3b4e7a
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-10-04 13:28:30 +0300

    Add HW1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const { specs } = require('./swagger');
+const swaggerUi = require('swagger-ui-express');
+
+
+app.get('/', (req, res) => res.send('Home Page'));
+
+app.get('/about', (req, res) => {
+  res.json({ message: 'This is About Page' });
+});
+
+app.use('/healthcheck', require('./routes/healthchecker'));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port  http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
+  console.log(`Docs available at http://localhost:${PORT}/docs`);
 });
